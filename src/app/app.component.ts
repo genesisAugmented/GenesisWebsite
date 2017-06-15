811a9390b8d6b1ed23be9609947b00b1bc5b267f
Long Roos <long@genesisaugmented.com>
2017-06-16 03:13:56 +1000

    Added Angular integration on html creature images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,17 +13,28 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 export class AppComponent {
 
   items: FirebaseListObservable<any[]>;
+  characters: FirebaseListObservable<any[]>;
+
   msgVal: string = '';
   myimage: string = 'http://i1002.photobucket.com/albums/af148/igard/planets/Desert-0010.png';
   itemname: string = 'Xitarea';
   itemlvl: string = '4';
   profile: boolean;
 
+  c_name: string = 'Osirus';
+  c_secondary: string = 'Intergalactic Sheriff';
+  c_img_url = 'http://i.imgur.com/DvqyCFn.png';
+  c_icon_url = '';
+
+
   constructor(public af: AngularFireDatabase) {
     this.items = af.list('/Planets');
     console.log(this.items)
 
-    this.profile = false
+    this.profile = true
+
+    this.characters = af.list('/Characters');
+
   }
 
   select(item: any) {
@@ -33,8 +44,13 @@ export class AppComponent {
     this.itemlvl = item.lvl
   }
 
-  planetpage() {
+  character_select(character: any) {
+    this.c_img_url = character.c_image_url;
+    this.c_name = character.c_name;
+    this.c_secondary = character.c_secondary;
+  }
 
+  planetpage() {
   	if(this.profile){
   		this.profile = false;
   	}else{
