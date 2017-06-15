1edf68f166ca2ef42ba85da36ee9e29e29ecb1cb
William Fischer <williamfischer363@gmail.com>
2017-06-15 19:35:32 +1000

    AngularJs Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    '.././animate.css'
+  ]
 })
+
 export class AppComponent {
-  title = 'app';
+
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+  myimage: string = 'http://i1002.photobucket.com/albums/af148/igard/planets/Desert-0010.png';
+  itemname: string = 'Xitarea';
+  itemlvl: string = '4';
+  profile: boolean;
+
+  constructor(public af: AngularFireDatabase) {
+    this.items = af.list('/Planets');
+    console.log(this.items)
+
+    this.profile = false
+  }
+
+  select(item: any) {
+    console.log(item)
+    this.myimage = item.URL
+    this.itemname = item.Name
+    this.itemlvl = item.lvl
+  }
+
+  planetpage() {
+
+  	if(this.profile){
+  		this.profile = false;
+  	}else{
+  		this.profile = true;
+  	}
+
+  	console.log(this.profile)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9jmBm_xYMviWKO4VFVre1jEKuuQ8FC4k",
+  authDomain: "genesis-1406e.firebaseapp.com",
+  databaseURL: "https://genesis-1406e.firebaseio.com/",
+  storageBucket: "gs://genesis-1406e.appspot.com"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
